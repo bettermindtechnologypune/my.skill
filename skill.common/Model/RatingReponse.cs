7fdd838c9c64be662c0e4536f3c6bf3aa01603a9
Better Mind technology <bettermindtechnologypune@gmail.com>
2021-11-10 18:12:50 +0530

    - Added model

---
+++ skill.common/Model/RatingReponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skill.common.Model
+{
+   public class RatingReponse
+   {
+      public Guid TaskId { get; set; }
+
+      public string TaskName { get; set; }
+
+      public int EmpRating { get; set; }
+
+      public Nullable<int> MangerRating { get; set; }
+   }
+}
