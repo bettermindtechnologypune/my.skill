50bb65c364ae73e288158adda15c02fbb91a2ff1
Better Mind technology <bettermindtechnologypune@gmail.com>
2021-11-10 18:21:45 +0530

    - Added rating name column

---
+++ skill.repostory/Migrations/20211108165313_ratingNameField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace skill.repository.Migrations
+{
+    public partial class ratingNameField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BUID",
+                table: "LevelTwo");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Rating",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Rating");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "BUID",
+                table: "LevelTwo",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+        }
+    }
+}
