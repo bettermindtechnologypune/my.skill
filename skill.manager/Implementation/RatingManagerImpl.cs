501878ee26121429a94ab48b88f4e2e0f948fdbb
Better Mind technology <bettermindtechnologypune@gmail.com>
2021-12-13 20:35:44 +0530

    - Removed Rating and Name from update rating

--- skill.manager/Implementation/RatingManagerImpl.cs
+++ skill.manager/Implementation/RatingManagerImpl.cs
@@ -155,10 +155,8 @@ namespace skill.manager.Implementation
             {
                var existingEnity = await _ratingRepository.GetAsync(resource.Id);
                existingEnity.ModifiedBy = UserId;
-               existingEnity.ModifiedDate = DateTime.UtcNow;
-               existingEnity.Name = resource.Name;
-               existingEnity.ManagerRating = resource.ManagerRating;
-               existingEnity.Rating = resource.Rating;            
+               existingEnity.ModifiedDate = DateTime.UtcNow;             
+               existingEnity.ManagerRating = resource.ManagerRating;                           
                entities.Add(existingEnity);
             }
             else
