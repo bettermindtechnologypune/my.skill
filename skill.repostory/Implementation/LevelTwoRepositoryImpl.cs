5592acee1f7fbe48f193edd50729a9bfe00f3708
Better Mind technology <bettermindtechnologypune@gmail.com>
2021-12-16 21:30:34 +0530

    Skill index fixes

--- skill.repostory/Implementation/LevelTwoRepositoryImpl.cs
+++ skill.repostory/Implementation/LevelTwoRepositoryImpl.cs
@@ -74,11 +74,12 @@ namespace skill.repository.Implementation
                             case when temp1.cal >=4.51 and temp1.cal  <= 5 then 5 END as L5,
                             case when temp1.cal >=0 and temp1.cal  <= 0.50 then 0 END as L0
                            from
-                           (select l2.Id DId, l2.Name, e.Id, sum(r.Rating * t.wattage)/100 cal from skill_db.leveltwo l2
-                           inner join skill_db.task t on t.levelId = l2.Id
-                           inner join skill_db.rating r on r.TaskId = t.Id
-                           inner join skill_db.employee e on e.Id = r.EmpId where l2.LevelOneId = @levelOneId
-                           group by e.Id) as temp1) as temp2 group by temp2.DId";
+                           (select l2.Id DId, l2.Name, e.Id, sum(r.Rating * t.wattage)/100 cal from skill_db.levelone l1
+                           right outer join skill_db.leveltwo l2 on l2.Id = l1.Id
+                           left join skill_db.task t on t.levelId = l2.Id
+                           left join skill_db.rating r on r.TaskId = t.Id
+                           left join skill_db.employee e on e.Id = r.EmpId where l2.LevelOneId = @levelOneId
+                           group by  e.Id, l2.Id) as temp1) as temp2 group by temp2.DId";
 
             using (var command = new MySqlCommand(query, Connection))
             {
