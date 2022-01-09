de8a5731ef5857735dbaccf435878320c1981a05
Better Mind technology <bettermindtechnologypune@gmail.com>
2022-01-09 18:13:13 +0530

    m

---
+++ skill.common/Model/ChangePassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skill.common.Model
+{
+   public class ChangePassword
+   {
+      public string ResetCode { get; set; }
+
+      [Required]
+      public string Email {get;set;}
+
+      [Required]
+      public string NewPassword { get; set; }
+
+      public string UserId { get; set; }
+   }
+}
---
+++ skill.repostory/Entity/PasswordResetRequestEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skill.repository.Entity
+{
+  public class PasswordResetRequestEntity : IBaseEntity
+   {
+      /// <summary>
+      ///  Unique identifier for a forgotten password reset request
+      /// </summary>
+      public Guid Id { get; set; }
+
+      /// <summary>
+      /// login name of user
+      /// </summary>
+      public string Login { get; set; }         
+
+      /// <summary>
+      /// Unique Identity of user
+      /// </summary>
+      public Guid UserId { get; set; }
+
+      public DateTime CreatedDate { get ; set; }
+
+      public string ResetCode { get; set; }
+   }
+}
